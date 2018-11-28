7416e1c95b86e6092e7a397073e5c046f5b60611
ronaldmirabal <ronaldmirabal@outlook.es>
2018-11-28 19:22:45 -0400

    Subida de proyecto

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace Lands  
+{
+    using Xamarin.Forms;
+    using Views;
+
+    public partial class App : Application
+    {
+        #region Construstor
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+        #endregion
+
+        #region Metodos
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
